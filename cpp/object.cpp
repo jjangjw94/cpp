aa2b9d4587067469e0bddeceb1a5d49cf781143a
jjangjw94 <wodnjs9402@naver.com>
2021-11-08 11:49:34 +0900

    test

--- cpp/object.cpp
+++ cpp/object.cpp
@@ -1,1 +1,38 @@
 
+#include <iostream>
+#include <vector>
+#include<string>
+#include<thread>
+#include<chrono>
+
+using namespace std;
+
+void PrintCurrentTime()
+{}
+
+void Printmessage(const std::string& message)
+{
+    std::cout << "sleep now...";
+    PrintCurrentTime();
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::cout << message << " ";
+    PrintCurrentTime();
+}
+
+int main()
+{
+    std::thread thread(Printmessage, "Message from a child thread.");
+
+    Printmessage("message from a main thread");
+    Printmessage("hello");
+    Printmessage("hello22");
+    Printmessage("hello33");
+    Printmessage("hello22");
+    Printmessage("hello");
+    thread.join();
+
+    return 0;
+
+}
+
