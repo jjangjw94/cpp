99f8b3ac443b2224a41040588c944a2730554850
user <user@DESKTOP-2EE06O8>
2021-11-08 09:58:27 +0900

    초기 커밋.

---
+++ cpp/object.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include<string>
+#include<thread>
+#include<chrono>
+
+using namespace std;
+
+void PrintCurrentTime()
+{}
+
+void Printmessage(const std::string& message)
+{
+    std::cout << "sleep now...";
+    PrintCurrentTime();
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::cout << message << " ";
+    PrintCurrentTime();
+}
+
+int main()
+{
+    std::thread thread(Printmessage, "Message from a child thread.");
+
+    Printmessage("message from a main thread");
+    thread.join();
+
+    return 0;
+
+}
